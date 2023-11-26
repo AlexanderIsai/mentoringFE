c641253ac2a535e3ceb03572da9c861ee46876b7
AlexandrIsai <artat@ukr.net>
2023-11-26 22:04:03 +0200

    26.11

---
+++ Yulija-DynamicArray/script2.js
@@ -0,0 +1,14 @@
+const getCoins = (sum, coins) => {
+    const result = [];
+
+    return result;
+};
+
+const main = () => {
+    const penny = [1, 3, 4, 2];
+    const amount = 5;
+    res = getCoins(amount, penny);
+    console.log(res);
+};
+
+main();
